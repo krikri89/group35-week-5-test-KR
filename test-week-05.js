598efa68d19db3589c6c6a8e8857aba40cc8c0f4
Kristina Rudyte <rkristina32@yahoo.com>
2022-04-11 19:32:03 +0300

    fixed test

--- test-week-05.js
+++ test-week-05.js
@@ -18,8 +18,8 @@ console.log("-----2--------");
 /*
 2. Naudojant for ciklą, išvesti į konsolę skaičius nuo 1 iki 10. (5 taškai)
 */
-for (i = 0; i < 10; i++) {
-  console.log(i + 1);
+for (i = 0; i < 11; i++) {
+  console.log(i);
 }
 
 console.log("---3----");
@@ -34,9 +34,12 @@ console.log("----4-----");
 /*
 4. Naudojant for ciklą, sugeneruoti penkis atsitiktinius skaičius nuo 1 iki 10. Išvesti juos konsolėje. (5 taškai)
 */
-for (let i = 0; i < 10; i++) {
-  const random = Math.floor(Math.random() * 10 + 1);
-  console.log(random);
+
+function rand(max, min) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+for (let i = 0; i < 5; i++) {
+  console.log(rand(1, 10));
 }
 
 console.log("----5-----");
@@ -45,7 +48,7 @@ console.log("----5-----");
 */
 let skaicius = 0;
 while (skaicius !== 5) {
-  skaicius = Math.floor(Math.random() * (10 - 1) + 1);
+  skaicius = rand(1, 10);
   console.log(skaicius);
 }
 
@@ -54,20 +57,56 @@ console.log("---6------");
 6. Sukurti masyvą, kurio ilgis būtų nuo 20 iki 30, o reikšmės būtų skaičiai nuo 10 iki 30. Surasti didžiausią masyvo reikšmę, NENAUDOJANT sort() bei Math.max() funkcijų. (7 taškai)
 */
 
-let nuo = 20;
-let iki = 30;
-const num = new Array(nuo, iki);
+let arr = [];
+let max = 9;
 
-console.log(num.length);
+for (let i = 0; i < rand(20, 30); i++) {
+  arr.push(rand(10, 30));
+}
+console.log(arr);
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > max) {
+    max = arr[i];
+  }
+}
+console.log("max:" + max);
 
 console.log("----7-----");
 /*
 7. Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, C ir D, o ilgis 100. Suskaičiuokite kiek yra kiekvienos raidės. (7 taškai)
 */
-// let sarasas = [ABCD]
-// sarasas.length = 100;
-// const random3 = Math.floor(Math.random() * 10 +1);
-// console.log(random3);
+let sarasas = ["A", "B", "C", "D"];
+const arr2 = [];
+
+for (let i = 0; i < 100; i++) {
+  arr2.push(sarasas[rand(0, 3)]);
+}
+
+console.log(arr2);
+
+let countA = 0;
+let countB = 0;
+let countC = 0;
+let countD = 0;
+
+for (let i = 0; i < arr2.length; i++) {
+  if (arr2[i] === "A") {
+    countA++;
+  }
+  if (arr2[i] === "B") {
+    countB++;
+  }
+  if (arr2[i] === "C") {
+    countC++;
+  }
+  if (arr2[i] === "D") {
+    countD++;
+  }
+}
+console.log(
+  `Raidžių masyve yra: A: ${countA}, B: ${countB}, C: ${countC}, D: ${countD}`
+);
 
 console.log("---8----");
 /*
@@ -76,58 +115,89 @@ Jei kintamieji skaičiai, grąžinti skaičių sumą, jei kintamieji masyvai - g
 */
 // let lygineSUma = 0;
 function lygineSuma(num1, num2) {
-  if (typeof num1 !== "number" || num2 !== "number") {
-        console.log("ERROR");
+  if (typeof num1 === "number" && num2 === "number") {
+    let suma = num1 + num2;
+    if (suma % 2 === 0) {
+      return `Skaiciu num1 ir num2 yra ${suma}`;
+    } else {
+      return "Skaiciu suma nelygine";
     }
-    if (typeof num1 !== "object" || num2 !== "object") {
-    console.log("ERROR");
-    // const suma = num1 + num2;
-  } else if ((num1 + num2) % 2 !== 0 || ([num1] + [num2]) % 2 !== 0) {
-    console.log(lygineSuma);
+  } else if (Array.isArray(num1) && Array.isArray(num2)) {
+    let suma = num1.length + num2.length;
+  if (suma % 2 === 0) {
+    return `Masyvu num1 and num2 suma yra ${suma}`;
   } else {
-    console.log("Suma nelygine");
+    return "Masyvu ilgiu suma nelygine";
+  }
+  } else {
+    return 'Pateikti neteisingi kintamieji'
   }
 }
 console.log(lygineSuma(2, 5));
+console.log(lygineSuma(2, 6));
+console.log(lygineSuma("2", 5));
+console.log(lygineSuma([2], 5));
+console.log(lygineSuma([2], [5]));
+console.log(lygineSuma([2], [5]));
+console.log(lygineSuma([2], [5, 2, "345", "1"]));
 
-console.log('----9----');
+console.log("----9----");
 /*
 9. Parašyti funkciją pirminisSkaicius. Funkcija turi vieną kintamąjį. Turi būti patikrinimas, kad kintamasis yra skaičius. Funkcija turi grąžinti ar pateiktas skaičius yra pirminis( pirminis skaičius yra tas, kuris dalinasi tik iš savęs ir tik iš vieneto be liekanos.) (10 taškų)
 */
 
 function pirminiaiSkaiciai(a) {
-    if (typeof a !== 'number') {
-        console.log('Not a number');
+  if (typeof a !== "number") {
+    console.log("Not a number");
+  }
+  let count = 0;
+  for (let i = 1; i <= a; i++) {
+    if (a % i === 0) {
+      count++;
+    }
+  }
+  if (count < 2) {
+    return "ivestas blogas skaicius";
+  }
+  if (count === 2) {
+    return "Ivestas skaicius yra pirminis";
+  }
+  if (count > 2) {
+    return "Ivestas skaicius ne pirminis";
+  }
 }
-if (a % a !==0 && a % 1 !==0) {
-    console.log('Skaicius ne pirminis');
-} 
-const number = a % a;
 
-return `${number} yra pirminis`
-}
+console.log(pirminiaiSkaiciai("abcd"));
+console.log(pirminiaiSkaiciai([4, 4, "abcd"]));
+console.log(pirminiaiSkaiciai(0));
+console.log(pirminiaiSkaiciai(1));
+console.log(pirminiaiSkaiciai(3));
+console.log(pirminiaiSkaiciai(15));
+console.log(pirminiaiSkaiciai(17));
 
-console.log(pirminiaiSkaiciai(9));
-
-console.log('----10 ----');
+console.log("----10 ----");
 /*
 10. Parašyti funkciją telefonoNumeris. Funkcija turi priimti vieną kintamąjį - masyvą. Masyvo elementai - skaičiai, ilgis - 10. Funkcija turi grąžinti telefono numerį tokiu formatu - 
 "(XXX) XXX-XXXX". (10 taškų)*/
-function telefonoNumeris(phone) {
-  if (typeof phone !== `object`) {
-    return `error`;
-  }
-  const stringPhone = "" + phone;
 
-  if (stringPhone.length < 10) {
+function telefonoNumeris(a) {
+  if (!Array.isArray(a)) {
+    return `Kintamasis nera masyvas`;
+  }
+  if (a.length !== 10) {
     return `Number missing`;
   }
-  if (phone < 0) {
-    return `Number cannot be negative`;
+  for (let i = 0; i < 10; i++) {
+    if (typeof a[i] !== "number") {
+      return "Masyvo reiksmes turi buti skaiciai";
+    }
   }
-  for (let i = 0; i < stringPhone.length; i++) {
-    const number = phone[i];
-  }
-  return stringPhone;
+  return `(${a[0]}${a[1]}${a[2]}) ${a[3]}${a[4]}${a[5]}-${a[6]}${a[7]}${a[8]}${a[9]}`;
 }
-console.log(telefonoNumeris[]);
+console.log(telefonoNumeris(17));
+console.log(telefonoNumeris("asdasd"));
+console.log(telefonoNumeris([9, 5, 8, 6, 5, 4, 7, 1, 2]));
+console.log(telefonoNumeris([9, 5, 8, 6, 5, 4, 7, 1, 2, 1, 8]));
+console.log(telefonoNumeris([9, 5, 8, 6, 5, 4, "7", 1, 2, 1, 8]));
+console.log(telefonoNumeris([9, 5, 8, 6, 5, 4, "7", 1, 2, 1]));
+console.log(telefonoNumeris([9, 5, 8, 6, 5, 4, 7, 1, 2, 1]));
